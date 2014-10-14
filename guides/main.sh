1563ad3a0fb8a0acaa23937ab94c26fa3a2ad74d
Yohei YASUKAWA <yasulab@gmail.com>
2014-10-14 14:35:47 +0900

    Add comments for manualy deploy

--- guides/main.sh
+++ guides/main.sh
@@ -5,3 +5,10 @@
 ruby allocate.rb
 rm -rf output
 bundle exec rake guides:generate:html
+
+# Then, manually type these commands:
+#   $ git add .
+#   $ git commit -m "Publish foo bar"
+#   $ git push origin japanese
+#   $ git push heroku japanese:master
+
