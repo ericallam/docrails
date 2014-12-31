896836fb304aa6cc32ed9b5f5061e112aa1adf1d
Yohei YASUKAWA <yasulab@gmail.com>
2014-12-31 13:22:43 +0900

    Fix typo and add usage/example to Check Typo script

--- guides/check_typo.rb
+++ guides/check_typo.rb
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 # Cited from コードの世界 by Matz
 # 典型的なスペル・ミスを探す方法, 図6 文章のミスをチェックするスクリプト
+#
+# Usage: ./check_typo.rb FILENAME
+#  e.g.: ./check_typo.rb source/testing.md
 
 ARGF.each do |line|
   print ARGF.file.path, " ",
     ARGF.file.lineno, ":",
-    line if line.gsub!(/([へにおはがでな])\1/s, '[[\&]]')
+    line if line.gsub!(/([へにおはがでな])\1/, '[[\&]]')
 end
