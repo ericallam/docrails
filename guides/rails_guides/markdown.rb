05076987c8c8f755d0faa0ff5a39ca0dc3dc90d4
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-05-12 16:00:49 +0900

    Move #render

--- guides/rails_guides/markdown.rb
+++ guides/rails_guides/markdown.rb
@@ -17,11 +17,9 @@ module RailsGuides
     def render(body)
       @raw_body = body
       extract_raw_header_and_body
-      extract_raw_body_and_references
       generate_header
       generate_title
       generate_body
-      generate_references
       generate_structure
       generate_index
       render_page
--- guides/rails_guides/markdown_ja.rb
+++ guides/rails_guides/markdown_ja.rb
@@ -2,5 +2,17 @@ require_relative 'markdown'
 
 module RailsGuides
   class MarkdownJa < Markdown
+    def render(body)
+      @raw_body = body
+      extract_raw_header_and_body
+      extract_raw_body_and_references
+      generate_header
+      generate_title
+      generate_body
+      generate_references
+      generate_structure
+      generate_index
+      render_page
+    end
   end
 end
