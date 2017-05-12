906e9b7bdbfb5005a7c450477113827f30bd54a0
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-05-12 12:38:56 +0900

    Move #documents_by_section

--- guides/rails_guides/helpers_ja.rb
+++ guides/rails_guides/helpers_ja.rb
@@ -3,5 +3,9 @@ require_relative 'helpers'
 module RailsGuides
   module HelpersJa
     include Helpers
+
+    def documents_by_section
+      @documents_by_section ||= YAML.load_file(File.expand_path("../../source/#@lang/documents.yaml", __FILE__))
+    end
   end
 end
