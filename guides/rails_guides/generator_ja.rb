6919ec131b7270e42cebd40720d7ef7d9cf3ed5a
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-06-21 17:47:10 +0900

    Replace output_dir method with instance value

--- guides/rails_guides/generator_ja.rb
+++ guides/rails_guides/generator_ja.rb
@@ -32,7 +32,7 @@ module RailsGuides
     def generate_docset
       require 'rails_guides/dash'
       docset_name = "ruby_on_rails_guides_#@version%s.docset" % (@lang.present? ? ".#@lang" : '')
-      Dash.generate(output_dir, docset_name)
+      Dash.generate(@output_dir, docset_name)
     end
 
     def initialize_dirs
