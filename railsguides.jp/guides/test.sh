639e6afd47079d046ccae491afdc74f17269df04
Yohei Yasukawa <yasulab@gmail.com>
2017-07-13 00:41:33 +0900

    Remove needless files in railsguides.jp dir

--- railsguides.jp/guides/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Test Markdown files using 'check_typo.rb'
-
-for article in `ls source/ja | grep md`
-do
-    echo "Testing ${article}"
-    ruby check_typo.rb "./source/ja/${article}"
-done
