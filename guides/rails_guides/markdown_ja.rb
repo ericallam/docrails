80213b036b49356f6b517354dc206bf568fbdb24
showw <matsuo@pxdesign.jp>
2019-10-28 21:26:31 +0900

    余白とレイアウトの調整

--- guides/rails_guides/markdown_ja.rb
+++ guides/rails_guides/markdown_ja.rb
@@ -59,8 +59,12 @@ module RailsGuides
             <h3 class="chapter"><img src="images/chapters_icon.gif" alt="" />目次</h3>
             #{@index}
             <aside id="toppage">
+              <div>
               <a href="https://railsguides.jp/pro"><img src="images/bnr-pro-plan.jpg" alt="RailsガイドProプラン" class="bnr-proplan" /></a>
+              </div>
+              <div>
               <a href="https://twitter.com/search?f=tweets&q=%20%23Rails%E3%82%AC%E3%82%A4%E3%83%89&src=typd&lang=ja" target="_blank" title="twitterへのリンク"><img src="images/btn-twitter.png" alt="みんなのつぶやき Railsガイド" /></a>
+              </div>
             </aside>
           </div>
           INDEX
