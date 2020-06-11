db9b429210df61f5a0f08311950d4bd4afc2e1ee
Yuki Mihashi <31533303+yuki384@users.noreply.github.com>
2020-06-11 16:12:55 +0900

    syntaxhighlighterを削除

--- guides/assets/javascripts/responsive-tables.js
+++ guides/assets/javascripts/responsive-tables.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var switched = false;
-  $("table").not(".syntaxhighlighter").addClass("responsive");
+  $("table").addClass("responsive");
   var updateTables = function() {
     if (($(window).width() < 767) && !switched ){
       switched = true;
@@ -16,24 +16,24 @@ $(document).ready(function() {
       });
     }
   };
-   
+
   $(window).load(updateTables);
   $(window).bind("resize", updateTables);
-   
-	
+
+
 	function splitTable(original)
 	{
 		original.wrap("<div class='table-wrapper' />");
-		
+
 		var copy = original.clone();
 		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
 		copy.removeClass("responsive");
-		
+
 		original.closest(".table-wrapper").append(copy);
 		copy.wrap("<div class='pinned' />");
 		original.wrap("<div class='scrollable' />");
 	}
-	
+
 	function unsplitTable(original) {
     original.closest(".table-wrapper").find(".pinned").remove();
     original.unwrap();
