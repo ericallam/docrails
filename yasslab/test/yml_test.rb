3d8df37bb443a0409ab017b300f94a414d6ee178
Shigenobu Nishikawa <shishi@srevo.net>
2021-08-13 16:49:54 +0900

    better test yml

--- yasslab/test/yml_test.rb
+++ yasslab/test/yml_test.rb
@@ -1,9 +1,13 @@
 require 'test/unit'
 
 class TestYaml < MiniTest::Unit::TestCase
-  def test_yaml_load
+  def test_exists_documents_yaml
+    assert(File.exists?('./guides/source/ja/documents.yaml', true))
+  end
+
+  def test_load_yaml
     assert_nothing_raised do
-      Psych.load_file('/home/shishi/dev/src/github.com/yasslab/railsguides.jp/guides/source/ja/documents.yaml')
+      Psych.load_file('./guides/source/ja/documents.yaml')
     end
   end
 end
